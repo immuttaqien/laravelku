d4552cee604e333c66d4b981eab8704529e700af
Ilham Muttaqien <mt.ilham@gmail.com>
2020-10-12 07:01:45 +0700

    Membuat encrypt dan decrypt data

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/enkripsi', 'EncryptController@enkripsi');
+Route::get('/data', 'EncryptController@data');
+Route::get('/data/{data_rahasia}', 'EncryptController@data_proses');
