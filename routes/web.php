568de622f52993b58c58a8b47a6360f14ae619c8
Ilham Muttaqien <mt.ilham@gmail.com>
2020-10-19 07:10:17 +0700

    Membuat multi bahasa localization

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,12 @@ Route::get('/pegawai', 'PegawaiController@index');
 Route::get('/pegawai/cetak_pdf', 'PegawaiController@cetak_pdf');
 Route::get('/pegawai/export_excel', 'PegawaiController@export_excel');
 Route::post('/pegawai/import_excel', 'PegawaiController@import_excel');
+
+Route::get('/form', function(){
+    return view('biodata');
+});
+
+Route::get('/{locale}/form', function ($locale) {
+    App::setLocale($locale);
+    return view('biodata');
+});
